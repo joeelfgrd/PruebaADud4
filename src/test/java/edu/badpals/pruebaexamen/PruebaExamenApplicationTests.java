495361675c9c6d7042705c944716ba0203a13565
Joel <joelfigueiridomolares@gmail.com>
2025-02-20 18:24:39 +0100

    First commit

---
+++ src/test/java/edu/badpals/pruebaexamen/PruebaExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.badpals.pruebaexamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaExamenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
